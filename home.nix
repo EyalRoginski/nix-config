ed588b698453f3fdcca2a9b3587d53f113d08c69
EyalRoginski <eyalrog1@gmail.com>
2025-06-01 11:03:20 +0000

    Tmux works

--- home.nix
+++ home.nix
@@ -23,7 +23,6 @@
     tmux
     tree
     zsh-powerlevel10k
-    tmuxPlugins.dracula
     gcc
     nodejs_24
   ];
@@ -32,6 +31,25 @@
   programs.zsh.initContent = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme\nsource ~/.p10k.zsh";
   home.file.".tmux.conf".source = "${inputs.dotfiles}/shell/.tmux.conf";
   home.file.".p10k.zsh".source = "${inputs.dotfiles}/shell/.p10k.zsh";
+
+  programs.tmux = {
+    enable = true;
+    plugins = with pkgs; [
+      tmuxPlugins.sensible
+      {
+        plugin = tmuxPlugins.dracula;
+        extraConfig = ''
+        # Dracula config
+            set -g @dracula-show-powerline true
+            set -g @dracula-show-left-icon session
+            set -g @dracula-plugins "time git battery"
+            set -g @dracula-show-timezone false
+            set -g @dracula-military-time true
+            set -g @dracula-show-empty-plugins false
+        '';
+      }
+    ];
+  };
 
   home.file.".config/nvim".source = "${inputs.dotfiles}/nvim/.config/nvim";
 
