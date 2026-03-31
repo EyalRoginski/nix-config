af460a40836697a9d5dc567177043452b0ff9bf6
EyalRoginski <eyalrog1@gmail.com>
2026-03-31 17:38:24 +0300

    More man pages

--- configuration.nix
+++ configuration.nix
@@ -43,6 +43,14 @@
     extraGroups = ["wheel"]; # Allows sudo
   };
 
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
+    man-pages
+    man-pages-posix
   ];
+
+  documentation = {
+    dev.enable = true;
+    man.generateCaches = true;
+    nixos.includeAllModules = true;
+  };
 }
